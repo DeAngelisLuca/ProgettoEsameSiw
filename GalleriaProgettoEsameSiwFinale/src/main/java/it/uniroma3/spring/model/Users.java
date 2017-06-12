c21bf555cbced053409aaaaf9985895430304501
Luca <lucadeangelis1995@hotmail.it>
2017-06-12 15:48:59 +0200

    aggiunto inserimento user

---
+++ GalleriaProgettoEsameSiwFinale/src/main/java/it/uniroma3/spring/model/Users.java
@@ -0,0 +1,67 @@
+package it.uniroma3.spring.model;
+import java.util.List;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+@Entity
+@Table(name="users")
+public class Users {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@Max(20)
+	@Min(4)
+	@NotNull
+	@Pattern(regexp = "^[A-Za-z0-9]*$")
+	private String username;
+
+	@Max(20)
+	@Min(4)
+	@NotNull
+	@Pattern(regexp = "^[A-Za-z0-9]*$")
+	private String password;
+
+	private boolean enabled;
+
+	
+
+	
+public Users(Long id ,String username, String password, boolean enabled) {
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.enabled = true;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+
+
+
+
+
+
+}
---
+++ GalleriaProgettoEsameSiwFinale/src/main/java/it/uniroma3/spring/repository/UsersRepository.java
@@ -0,0 +1,11 @@
+package it.uniroma3.spring.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import it.uniroma3.spring.model.*;
+
+public interface UsersRepository extends CrudRepository<Users, Long>{
+
+}
